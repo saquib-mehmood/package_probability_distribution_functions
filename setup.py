8df6bca6f4feca0c4a352d0133b897208bcfa9cf
saquib-mehmood <62308209+saquib-mehmood@users.noreply.github.com>
2020-07-26 18:13:21 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(name='pyproblib',
+      version='1.1',
+      description='Probability Distribution Functions',
+      packages=['pyproblib'],
+      author='Saquib Mehmood',
+      author_email='indowhales@protonmail.com',
+      zip_safe=False)
